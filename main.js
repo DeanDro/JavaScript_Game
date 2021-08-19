27819b77e22bd9785c9afd6dcb45f2e0eb1d5f28
Konstantinos Drosos <dean.drossos@gmail.com>
2021-08-19 04:49:32 -0400

    Word puzzle first approach

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+
+let win = null;
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() =>{
+    createWindow();
+})
+
+app.on('window-all-closed', function(){
+    if (process.platform != 'darwin') app.quit();
+})
